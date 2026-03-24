5ca1e1a27d10cc8b5369b31d844dce3f970bb5c9
Yurs13 <yairsantaflow@gmail.com>
2026-03-23 20:17:01 -0600

    Practica 1.1

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+// https:// -> protocolo
+// www.yputube.com/ -> Dominio
+// watch -> layput
+// ? -> envio de variables por metodo get
+// v= -> definir variables en metodo get
+// 7LD9 - wofjIs -> valor de la variable 
+
+$nombre = "Yurs";
+$apellido = "Rivera";
+$edad = "25 años";
+$estatura = "1.83";
+$peso = "110 kilos";
+$cabello = "Castaño";
+$colorFav = "Azul";
+$ojos = "cafes";
+$cejas = "pobladas";
+$pestañas = "largas";
+
+echo "Hola mundo" .$nombre + .$apellido + .$edad + .$estatura + .$peso + .$cabello + .$colorFav + .$ojos + .$cejas + .$pestañas;
+;
+?>
